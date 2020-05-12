9ea76211d68558bc8eb1131b0ace29d30cbcc00c
zero-bugs <xiaokktaoqi.2008@163.com>
2020-05-12 23:57:42 +0800

    tree store by array

--- src/test/java/com/zero/bugs/demo/MainTest.java
+++ src/test/java/com/zero/bugs/demo/MainTest.java
@@ -1,7 +1,20 @@
 package com.zero.bugs.demo;
 
+import com.zero.bugs.demo.algorithm.repository.ArrayBinTree;
+
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("hello world.");
+
+        ArrayBinTree<String> binTree = new ArrayBinTree<>(4, "根");
+
+        binTree.add(0, "0右", false);
+        binTree.add(2, "2右", false);
+        binTree.add(2, "2左", true);
+        binTree.add(0, "0左", true);
+        binTree.add(1, "1左", true);
+
+        System.out.println(binTree);
+        System.out.println(binTree.getLeft(2));
+        System.out.println(binTree.getParent(6));
     }
 }
